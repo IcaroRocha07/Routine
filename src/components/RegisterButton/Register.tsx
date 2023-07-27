126f0319a40ef8ebfc0a8f896d3d9fec40fb6f14
Icaro Rocha <icarorocha02@outlook.com>
2023-07-26 22:19:53 -0300

    ola

--- src/components/RegisterButton/Register.tsx
+++ src/components/RegisterButton/Register.tsx
@@ -3,10 +3,13 @@ import * as S from "./Register.styles";
 import { IRegister } from './Register.interface';
 
 const Register: React.FC<IRegister> = ({onPress}) => {
+
+  console.log('olá')
     return (
         <S.RegisterContainer onPress={onPress}>
           <S.Image source={require('.././../../assets/mais.png')}/>
         </S.RegisterContainer>
+        
     )
 }
 
